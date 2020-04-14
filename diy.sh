f67afab3016e5d2b4d49e7948a116ea95c173d5a
wolfyu1991 <wolfyu1991@qq.com>
2020-04-14 16:29:59 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -15,7 +15,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # sed -i "s/OpenWrt /wolfyu build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
